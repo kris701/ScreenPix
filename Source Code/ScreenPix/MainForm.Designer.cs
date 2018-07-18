6f3da37dfdfff4251a16aaf6b891ba66259da87d
Kristian Skov <kris701kj@gmail.com>
2018-07-19 00:49:37 +0200

    Initial contribution

---
+++ Source Code/ScreenPix/MainForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ScreenPixNameSpace._0
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ColorLabel = new System.Windows.Forms.Label();
+            this.ImageWindowPictureBox = new System.Windows.Forms.PictureBox();
+            this.ColorShowPanel = new System.Windows.Forms.Panel();
+            this.ImageWindowPictureBoxPanel = new System.Windows.Forms.Panel();
+            this.ImageWindowPictureBoxLabel = new System.Windows.Forms.Label();
+            this.ColorLabelLabel = new System.Windows.Forms.Label();
+            this.ColorLabelPanel = new System.Windows.Forms.Panel();
+            this.ColorShowPanelLabel = new System.Windows.Forms.Label();
+            this.ColorShowPanelPanel = new System.Windows.Forms.Panel();
+            this.PositionLabelLabel = new System.Windows.Forms.Label();
+            this.PositionLabelPanel = new System.Windows.Forms.Panel();
+            this.PositionLabel = new System.Windows.Forms.Label();
+            this.CopyrightLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageWindowPictureBox)).BeginInit();
+            this.ImageWindowPictureBoxPanel.SuspendLayout();
+            this.ColorLabelPanel.SuspendLayout();
+            this.ColorShowPanelPanel.SuspendLayout();
+            this.PositionLabelPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ColorLabel
+            // 
+            this.ColorLabel.AutoSize = true;
+            this.ColorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ColorLabel.Location = new System.Drawing.Point(6, 7);
+            this.ColorLabel.Name = "ColorLabel";
+            this.ColorLabel.Size = new System.Drawing.Size(83, 72);
+            this.ColorLabel.TabIndex = 0;
+            this.ColorLabel.Text = "Red:    0\r\nGreen: 0\r\nBlue:   0";
+            // 
+            // ImageWindowPictureBox
+            // 
+            this.ImageWindowPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ImageWindowPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImageWindowPictureBox.Location = new System.Drawing.Point(9, 10);
+            this.ImageWindowPictureBox.Name = "ImageWindowPictureBox";
+            this.ImageWindowPictureBox.Size = new System.Drawing.Size(127, 127);
+            this.ImageWindowPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImageWindowPictureBox.TabIndex = 1;
+            this.ImageWindowPictureBox.TabStop = false;
+            // 
+            // ColorShowPanel
+            // 
+            this.ColorShowPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorShowPanel.Location = new System.Drawing.Point(8, 10);
+            this.ColorShowPanel.Name = "ColorShowPanel";
+            this.ColorShowPanel.Size = new System.Drawing.Size(127, 127);
+            this.ColorShowPanel.TabIndex = 2;
+            // 
+            // ImageWindowPictureBoxPanel
+            // 
+            this.ImageWindowPictureBoxPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImageWindowPictureBoxPanel.Controls.Add(this.ImageWindowPictureBox);
+            this.ImageWindowPictureBoxPanel.Location = new System.Drawing.Point(12, 22);
+            this.ImageWindowPictureBoxPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.ImageWindowPictureBoxPanel.Name = "ImageWindowPictureBoxPanel";
+            this.ImageWindowPictureBoxPanel.Size = new System.Drawing.Size(147, 147);
+            this.ImageWindowPictureBoxPanel.TabIndex = 3;
+            // 
+            // ImageWindowPictureBoxLabel
+            // 
+            this.ImageWindowPictureBoxLabel.AutoSize = true;
+            this.ImageWindowPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImageWindowPictureBoxLabel.Location = new System.Drawing.Point(20, 13);
+            this.ImageWindowPictureBoxLabel.Name = "ImageWindowPictureBoxLabel";
+            this.ImageWindowPictureBoxLabel.Size = new System.Drawing.Size(40, 15);
+            this.ImageWindowPictureBoxLabel.TabIndex = 5;
+            this.ImageWindowPictureBoxLabel.Text = "Point";
+            // 
+            // ColorLabelLabel
+            // 
+            this.ColorLabelLabel.AutoSize = true;
+            this.ColorLabelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ColorLabelLabel.Location = new System.Drawing.Point(179, 13);
+            this.ColorLabelLabel.Name = "ColorLabelLabel";
+            this.ColorLabelLabel.Size = new System.Drawing.Size(86, 15);
+            this.ColorLabelLabel.TabIndex = 7;
+            this.ColorLabelLabel.Text = "Color values";
+            // 
+            // ColorLabelPanel
+            // 
+            this.ColorLabelPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorLabelPanel.Controls.Add(this.ColorLabel);
+            this.ColorLabelPanel.Location = new System.Drawing.Point(171, 22);
+            this.ColorLabelPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.ColorLabelPanel.Name = "ColorLabelPanel";
+            this.ColorLabelPanel.Size = new System.Drawing.Size(144, 82);
+            this.ColorLabelPanel.TabIndex = 6;
+            // 
+            // ColorShowPanelLabel
+            // 
+            this.ColorShowPanelLabel.AutoSize = true;
+            this.ColorShowPanelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ColorShowPanelLabel.Location = new System.Drawing.Point(331, 13);
+            this.ColorShowPanelLabel.Name = "ColorShowPanelLabel";
+            this.ColorShowPanelLabel.Size = new System.Drawing.Size(41, 15);
+            this.ColorShowPanelLabel.TabIndex = 9;
+            this.ColorShowPanelLabel.Text = "Color";
+            // 
+            // ColorShowPanelPanel
+            // 
+            this.ColorShowPanelPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorShowPanelPanel.Controls.Add(this.ColorShowPanel);
+            this.ColorShowPanelPanel.Location = new System.Drawing.Point(323, 22);
+            this.ColorShowPanelPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.ColorShowPanelPanel.Name = "ColorShowPanelPanel";
+            this.ColorShowPanelPanel.Size = new System.Drawing.Size(147, 147);
+            this.ColorShowPanelPanel.TabIndex = 8;
+            // 
+            // PositionLabelLabel
+            // 
+            this.PositionLabelLabel.AutoSize = true;
+            this.PositionLabelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PositionLabelLabel.Location = new System.Drawing.Point(179, 105);
+            this.PositionLabelLabel.Name = "PositionLabelLabel";
+            this.PositionLabelLabel.Size = new System.Drawing.Size(84, 15);
+            this.PositionLabelLabel.TabIndex = 9;
+            this.PositionLabelLabel.Text = "Coordinates";
+            // 
+            // PositionLabelPanel
+            // 
+            this.PositionLabelPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PositionLabelPanel.Controls.Add(this.PositionLabel);
+            this.PositionLabelPanel.Location = new System.Drawing.Point(171, 112);
+            this.PositionLabelPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.PositionLabelPanel.Name = "PositionLabelPanel";
+            this.PositionLabelPanel.Size = new System.Drawing.Size(144, 57);
+            this.PositionLabelPanel.TabIndex = 8;
+            // 
+            // PositionLabel
+            // 
+            this.PositionLabel.AutoSize = true;
+            this.PositionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PositionLabel.Location = new System.Drawing.Point(6, 7);
+            this.PositionLabel.Name = "PositionLabel";
+            this.PositionLabel.Size = new System.Drawing.Size(44, 48);
+            this.PositionLabel.TabIndex = 0;
+            this.PositionLabel.Text = "X: 0\r\nY: 0";
+            // 
+            // CopyrightLabel
+            // 
+            this.CopyrightLabel.AutoSize = true;
+            this.CopyrightLabel.Location = new System.Drawing.Point(3, 0);
+            this.CopyrightLabel.Name = "CopyrightLabel";
+            this.CopyrightLabel.Size = new System.Drawing.Size(130, 13);
+            this.CopyrightLabel.TabIndex = 10;
+            this.CopyrightLabel.Text = "© Kristian Skov Johansen";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 177);
+            this.Controls.Add(this.PositionLabelLabel);
+            this.Controls.Add(this.ColorShowPanelLabel);
+            this.Controls.Add(this.PositionLabelPanel);
+            this.Controls.Add(this.ColorShowPanelPanel);
+            this.Controls.Add(this.ColorLabelLabel);
+            this.Controls.Add(this.ColorLabelPanel);
+            this.Controls.Add(this.ImageWindowPictureBoxLabel);
+            this.Controls.Add(this.ImageWindowPictureBoxPanel);
+            this.Controls.Add(this.CopyrightLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(493, 216);
+            this.MinimumSize = new System.Drawing.Size(493, 216);
+            this.Name = "MainForm";
+            this.Text = "ScreenPix";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageWindowPictureBox)).EndInit();
+            this.ImageWindowPictureBoxPanel.ResumeLayout(false);
+            this.ColorLabelPanel.ResumeLayout(false);
+            this.ColorLabelPanel.PerformLayout();
+            this.ColorShowPanelPanel.ResumeLayout(false);
+            this.PositionLabelPanel.ResumeLayout(false);
+            this.PositionLabelPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ColorLabel;
+        private System.Windows.Forms.PictureBox ImageWindowPictureBox;
+        private System.Windows.Forms.Panel ColorShowPanel;
+        private System.Windows.Forms.Panel ImageWindowPictureBoxPanel;
+        private System.Windows.Forms.Label ImageWindowPictureBoxLabel;
+        private System.Windows.Forms.Label ColorLabelLabel;
+        private System.Windows.Forms.Panel ColorLabelPanel;
+        private System.Windows.Forms.Label ColorShowPanelLabel;
+        private System.Windows.Forms.Panel ColorShowPanelPanel;
+        private System.Windows.Forms.Label PositionLabelLabel;
+        private System.Windows.Forms.Panel PositionLabelPanel;
+        private System.Windows.Forms.Label PositionLabel;
+        private System.Windows.Forms.Label CopyrightLabel;
+    }
+}
+
---
+++ Source Code/ScreenPix/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace ScreenPixNameSpace._0
+{
+    public partial class MainForm : Form
+    {
+        #region Variabels
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        Bitmap ImageWindow = new Bitmap( 50, 50, PixelFormat.Format32bppArgb);
+        Point Pointer = new Point();
+        Pen BlackPen = new Pen(Color.Black, 1);
+        Graphics GFXScreenshot;
+        Bitmap ScreenPixelColor = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                ImageWindowPictureBox.Image = GenerateBitMap();
+
+                var Color = GetColorAt(Cursor.Position);
+
+                ColorLabel.Text =
+                    "Red: " + Color.R.ToString() +
+                    Environment.NewLine +
+                    "Green: " + Color.G.ToString() +
+                    Environment.NewLine +
+                    "Blue: " + Color.B.ToString();
+                PositionLabel.Text =
+                    "X: " + Cursor.Position.X +
+                    Environment.NewLine +
+                    "Y: " + Cursor.Position.Y;
+
+                ColorShowPanel.BackColor = Color.FromArgb(Color.R, Color.G, Color.B);
+
+                await Task.Delay(10);
+            }
+        }
+
+        Bitmap GenerateBitMap()
+        {
+            using (GFXScreenshot = Graphics.FromImage(ImageWindow))
+            {
+                if (Cursor.Position.X < 25)
+                    Pointer.X = 0;
+                else
+                    if (Cursor.Position.X + 25 > SystemInformation.VirtualScreen.Width)
+                        Pointer.X = SystemInformation.VirtualScreen.Width - 50;
+                    else
+                        Pointer.X = Cursor.Position.X - 25;
+                if (Cursor.Position.Y < 25)
+                    Pointer.Y = 0;
+                else
+                    if (Cursor.Position.Y + 25 > SystemInformation.VirtualScreen.Height)
+                        Pointer.Y = SystemInformation.VirtualScreen.Height - 50;
+                    else
+                        Pointer.Y = Cursor.Position.Y - 25;
+
+                GFXScreenshot.CopyFromScreen( Pointer.X, Pointer.Y, 0, 0, new Size(ImageWindow.Width, ImageWindow.Height), CopyPixelOperation.SourceCopy);
+
+                GFXScreenshot.DrawRectangle(BlackPen, ImageWindow.Width / 2 - 1, ImageWindow.Height / 2 - 1, 2, 2);
+            }
+            GFXScreenshot.Dispose();
+            return ImageWindow;
+        }
+
+        public Color GetColorAt(Point _Location)
+        {
+            using (Graphics GraphicsDestination = Graphics.FromImage(ScreenPixelColor))
+            {
+                using (Graphics GraphicsSource = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr ScreenDC = GraphicsSource.GetHdc();
+                    IntPtr HDC = GraphicsDestination.GetHdc();
+                    int retval = BitBlt(HDC, 0, 0, 1, 1, ScreenDC, _Location.X, _Location.Y, (int)CopyPixelOperation.SourceCopy);
+                    GraphicsDestination.ReleaseHdc();
+                    GraphicsSource.ReleaseHdc();
+                }
+            }
+
+            return ScreenPixelColor.GetPixel(0, 0);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y - 1);
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y + 1);
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                Cursor.Position = new Point(Cursor.Position.X - 1, Cursor.Position.Y);
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y);
+            }
+        }
+    }
+}
